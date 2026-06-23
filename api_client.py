c0ff344f0cd23f4c3e09caa2afb66bab5a51d859
hikeong <s25048@gsm.hs.kr>
2026-06-23 17:46:19 +0900

    feat: time import 추가

--- api_client.py
+++ api_client.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from urllib.error import HTTPError
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+from datetime import datetime, timezone
  
 from dotenv import load_dotenv
  
@@ -185,3 +186,15 @@ def fetch_enrollment_counts(lectures=None):
         }
         for lecture in lectures
     }
+
+def _parse_datetime(value):
+    if not value or not isinstance(value, str):
+        return None
+
+    try:
+        dt = datetime.fromisoformat(value.replace("Z", "+00:00"))
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=timezone.utc)
+        return dt
+    except ValueError:
+        return None
